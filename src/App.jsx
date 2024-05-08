384fee5294302013bdc2c472c9bf935fd327102e
Nicolas Gaviola <nicolgaviola@gmail.com>
2024-05-08 22:50:17 +0800

    unang yugto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from './components/Navbar'
+import Body from './components/Body'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Body/>
+    
+    </div>
+  )
+}
+
+export default App
