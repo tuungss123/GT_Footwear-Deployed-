7d01502d746dab2fc2fe1bab26ab871c2a3113c4
Nicolas Gaviola <nicolgaviola@gmail.com>
2024-05-28 01:19:08 +0800

    New Arrivals Page  added add to Cart icon and Header

--- backend/gt_footwear/gt_footwear/settings.py
+++ backend/gt_footwear/gt_footwear/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gt_footwear',
         'USER': 'root',
-        'PASSWORD': '12345',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '3306',
     }
--- frontend/src/pages/new-arrivals/index.jsx
+++ frontend/src/pages/new-arrivals/index.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const NewArrivals = () => {
     const [products, setProducts] = useState([]);
@@ -14,18 +16,45 @@ const NewArrivals = () => {
             });
     }, []);
 
+    const addToCart = async (product) => {
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/gt/cart/add/', {
+                product: product.id,
+                quantity: 1,
+                size: 1 
+            },{
+                withCredentials:true
+            });
+            console.log('Product added to cart:', response.data);
+        } catch (error) {
+            console.error('Error adding product to cart:', error);
+        }
+    };
+
     return (
-        <div className="h-auto flex flex-wrap m-10">
-            {products.slice(-5).map(product => (
-                <div key={product.id} className="w-[200px] h-[320px] rounded overflow-hidden shadow-lg m-4">
-                    <img className="w-[200px]" src={product.picture_url} alt={product.name} />
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-md mb-2">{product.name}</div>
-                        <p className="text-gray-700 text-sm">{product.brand_name}</p>
-                        <p className="text-gray-900 text-sm font-bold">{product.price} PHP</p>
-                    </div>
+        <div className="flex justify-center">
+            <div className="w-full max-w-screen-lg">
+                <div className='font-bold text-3xl mb-4 ml-4'>New Arrivals</div>
+                <div className="h-auto flex flex-wrap justify-center">
+                    {products.slice(-5).map(product => (
+                        <div key={product.id} className="w-[200px] h-[320px] rounded overflow-hidden shadow-lg m-2">
+                            <img className="w-[200px]" src={product.picture_url} alt={product.name} />
+                            <div className="px-6 py-4">
+                                <div className="font-bold text-md mb-2">{product.name}</div>
+                                <p className="text-gray-700 text-sm">{product.brand_name}</p>
+                                <div className='flex flex-row justify-between'>
+                                    <p className="text-gray-900 text-sm font-bold">{product.price} PHP</p>
+                                    <FontAwesomeIcon 
+                                        icon={faShoppingCart} 
+                                        style={{ fontSize: '20px', color: 'gray', cursor: 'pointer' }} 
+                                        onClick={() => addToCart(product)} 
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            </div>
         </div>
     );
 }
