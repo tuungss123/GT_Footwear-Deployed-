01cfce7339ffdf7b1f3a4b468125a7dc66599cf0
Nicolas Gaviola <nicolgaviola@gmail.com>
2024-06-12 22:52:08 +0800

    Refactor New Arrivals Page

--- frontend/src/pages/new-arrivals/index.jsx
+++ frontend/src/pages/new-arrivals/index.jsx
@@ -36,12 +36,13 @@ const NewArrivals = () => {
             <div className="w-full max-w-screen-lg">
                 <div className='font-bold text-3xl mb-4 ml-4'>New Arrivals</div>
                 <div className="h-auto flex flex-wrap justify-center">
-                    {products.slice(-5).map(product => (
+                    {products.slice(-10).map(product => (
                         <div key={product.id} className="w-[200px] h-[320px] rounded overflow-hidden shadow-lg m-2">
                             <img className="w-[200px]" src={product.picture_url} alt={product.name} />
                             <div className="px-6 py-4">
                                 <div className="font-bold text-md mb-2">{product.name}</div>
                                 <p className="text-gray-700 text-sm">{product.brand_name}</p>
+                                <p className="text-gray-700 text-sm">{product.gender}</p>
                                 <div className='flex flex-row justify-between'>
                                     <p className="text-gray-900 text-sm font-bold">{product.price} PHP</p>
                                     <FontAwesomeIcon 
