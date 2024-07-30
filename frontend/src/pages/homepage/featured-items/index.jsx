6d9177e5f9a7f25692a4862906c2440ce8b61026
Nicolas Gaviola <nicolgaviola@gmail.com>
2024-07-30 22:50:35 +0800

    homepage featured-items refactor(CARDS)

--- frontend/src/pages/homepage/featured-items/index.jsx
+++ frontend/src/pages/homepage/featured-items/index.jsx
@@ -1,40 +1,55 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 const Featured = () => {
   return (
     <div className="p-4">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 ">
         {/* New Releases Section */}
-        <div className="col-span-1 flex flex-col items-center">
+        <div className="relative max-w-xl mx-auto group">
+          <img
+            src="https://cdn.realsport101.com/images/ncavvykf/realsport-production/664c4020f06cb7627a0e61f6708ca71d96802a7c-1200x804.jpg?w=1200&h=804&auto=format"
+            alt="New Releases"
+            className="h-auto w-full object-cover rounded-md transition duration-300 transform group-hover:scale-105 group-hover:brightness-50"
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <Link to="/new-arrivals" className="text-white text-3xl font-bold pt-9 transition duration-300 relative">
+              New Arrivals
+              <span className="block w-full h-1 bg-white absolute  left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
+            </Link>
+          </div>
+        </div>
+
+
+
+        {/* Sneakers Section */}
+        <div className="relative max-w-xl mx-auto group">
           <img
             src="https://www.theeditldn.com/cdn/shop/articles/Image_6_213fd1aa-48e7-4ca0-ae61-e108eb8d8037.jpg?v=1670860572"
-            alt="New Releases"
-            className="w-full h-auto rounded-lg shadow-lg object-cover"
+            alt="Sneakers"
+            className="h-auto w-full object-cover rounded-md transition duration-300 transform group-hover:scale-105 group-hover:brightness-50"
           />
-          <Link to="/new-arrivals" className="mt-4 text-xl font-bold">New Arrivals →</Link>
-        </div>
-        
-        {/* Sneakers and Streetwear Sections */}
-        <div className="col-span-2 grid grid-cols-1 md:grid-cols-2 gap-4">
-          {/* Sneakers Section */}
-          <div className="flex flex-col items-center">
-            <img
-              src="https://cdn.sanity.io/images/c1chvb1i/production/d81ea71db00f3667ec984b3c43b04b79616d64db-2000x1336.jpg"
-              alt="Sneakers"
-              className="w-full h-auto rounded-lg shadow-lg object-cover"
-            />
-            <Link to="/mens" className="mt-4 text-xl font-bold">Sneakers →</Link>
+          <div className="absolute inset-0 flex items-center justify-center">
+            <Link to="/sneakers" className="text-white text-3xl font-bold pt-9 transition duration-300 relative">
+              Sneakers
+              <span className="block w-full h-1 bg-white absolute  left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
+            </Link>
           </div>
-          
-          {/* Streetwear Section */}
-          <div className="flex flex-col items-center">
-            <img
-              src="https://www.highsnobiety.com/static-assets/dato/1690970245-inside-nigerias-booming-streetwear-renaissance-07.jpg"
-              alt="Streetwear"
-              className="w-full h-auto rounded-lg shadow-lg object-cover"
-            />
-            <Link to="/streetwear" className="mt-4 text-xl font-bold">Streetwear →</Link>
+        </div>
+
+        {/* Streetwear Section */}
+        <div className="relative max-w-xl mx-auto group">
+          <img
+            src="https://www.theeditldn.com/cdn/shop/articles/Image_6_213fd1aa-48e7-4ca0-ae61-e108eb8d8037.jpg?v=1670860572"
+            alt="Apparels"
+            className="h-auto w-full object-cover rounded-md transition duration-300 transform group-hover:scale-105 group-hover:brightness-50"
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <Link to="/apparels" className="text-white text-3xl font-bold pt-9 transition duration-300 relative">
+              Apparels
+              <span className="block w-full h-1 bg-white absolute  left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
+            </Link>
           </div>
         </div>
       </div>
