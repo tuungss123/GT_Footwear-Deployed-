7cae72edf686f040a19b72b426bd65ad0410db3a
Nicolas Gaviola <nicolgaviola@gmail.com>
2024-08-06 09:52:44 +0800

    Added Form(Billing Details)

--- frontend/src/pages/cart/index.jsx
+++ frontend/src/pages/cart/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Cart = () => {
     const [cart, setCart] = useState([]);
     const [selectedItems, setSelectedItems] = useState([]);
@@ -14,18 +15,16 @@ const Cart = () => {
             .catch(error => {
                 console.error('Error fetching Cart Items:', error);
             });
-    }
+    };
 
     useEffect(() => {
         fetchCartItems();
     }, []);
 
-
-    const handleDelete =  async (id) => 
-         {
-            await axios.delete(`http://127.0.0.1:8000/gt/cart/remove/${id}`,{withCredentials:true})
-            fetchCartItems()
-        }
+    const handleDelete = async (id) => {
+        await axios.delete(`http://127.0.0.1:8000/gt/cart/remove/${id}`, { withCredentials: true });
+        fetchCartItems();
+    };
 
     const handleSelectAll = (event) => {
         if (event.target.checked) {
@@ -41,39 +40,31 @@ const Cart = () => {
         } else {
             setSelectedItems([...selectedItems, id]);
         }
-        
-    };  
+    };
+
     const handleAddQuantity = (id, quantity) => {
         (async () => {
             try {
                 const newQuantity = parseInt(quantity, 10) + 1;
-    
-                await axios.put(`http://127.0.0.1:8000/gt/cart/update/${id}/`, 
-                {quantity: newQuantity},
-                {withCredentials: true});
-                            fetchCartItems()
+                await axios.put(`http://127.0.0.1:8000/gt/cart/update/${id}/`, { quantity: newQuantity }, { withCredentials: true });
+                fetchCartItems();
             } catch (error) {
                 console.error('Error updating item quantity:', error);
             }
         })();
-    }
+    };
+
     const handleMinusQuantity = (id, quantity) => {
         (async () => {
             try {
                 const newQuantity = parseInt(quantity, 10) - 1;
-    
-                await axios.put(`http://127.0.0.1:8000/gt/cart/update/${id}/`,
-                {quantity: newQuantity },
-                { withCredentials: true });
-                          fetchCartItems()
+                await axios.put(`http://127.0.0.1:8000/gt/cart/update/${id}/`, { quantity: newQuantity }, { withCredentials: true });
+                fetchCartItems();
             } catch (error) {
                 console.error('Error updating item quantity:', error);
             }
         })();
-    }
-    
-    
- 
+    };
 
     return (
         <div className="flex flex-col md:flex-row p-6 bg-gray-100 min-h-screen">
@@ -81,9 +72,9 @@ const Cart = () => {
                 <h1 className="text-3xl font-bold mb-4">Cart({cart.length})</h1>
                 <div className="flex justify-between items-center border-b pb-4 mb-4">
                     <label className="flex items-center">
-                        <input 
-                            type="checkbox" 
-                            className="mr-2" 
+                        <input
+                            type="checkbox"
+                            className="mr-2"
                             onChange={handleSelectAll}
                             checked={selectedItems.length === cart.length}
                         />
@@ -92,9 +83,9 @@ const Cart = () => {
                 </div>
                 {cart.map(item => (
                     <div key={item.id} className="flex flex-col md:flex-row items-center justify-between bg-white p-4 mb-4 shadow-lg rounded-lg">
-                        <input 
-                            type="checkbox" 
-                            className="mr-2" 
+                        <input
+                            type="checkbox"
+                            className="mr-2"
                             checked={selectedItems.includes(item.id)}
                             onChange={() => handleSelectItem(item.id)}
                         />
@@ -109,30 +100,93 @@ const Cart = () => {
                                     <button className="pr-1 text-gray-600 hover:text-black" onClick={() => handleAddQuantity(item.id, item.quantity)}>+</button>
                                     <button className="text-gray-600 hover:text-black" disabled={item.quantity === 1} onClick={() => handleMinusQuantity(item.id, item.quantity)}>-</button>
                                 </div>
-                            </div>                        
+                            </div>
                         </div>
                         <div className="text-right">
                             <p className="text-xl font-bold">₱{item.product.price}</p>
                         </div>
-                        <button 
-                            onClick={() => handleDelete(item.id)} 
+                        <button
+                            onClick={() => handleDelete(item.id)}
                             className="ml-4 text-red-500 hover:text-red-700"
                         >
-                             <FontAwesomeIcon icon={faTrash} />
+                            <FontAwesomeIcon icon={faTrash} />
                         </button>
                     </div>
                 ))}
             </div>
 
-            <div className="w-full md:w-1/3 p-4 bg-white shadow-lg rounded-lg mt-4 md:mt-0 h-auto md:h-40">
-                <h2 className="text-2xl font-bold mb-4">Summary</h2>
+            <div className="w-full md:w-1/3 p-4 bg-white shadow-lg rounded-lg mt-4 md:mt-0 h-auto md:h-auto">
+                <h2 className="text-2xl font-bold mb-4">Billing Details</h2>
+                <form className="space-y-4">
+                    <div className="flex flex-col">
+                        <label className="mb-1 font-semibold">First Name</label>
+                        <input
+                            name="fullName"
+                            type="text"
+                            placeholder="Enter your First Name"
+                            className="p-2 border border-gray-300 rounded"
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="mb-1 font-semibold">Second Name</label>
+                        <input
+                            name="fullName"
+                            type="text"
+                            placeholder="Enter your Second Name"
+                            className="p-2 border border-gray-300 rounded"
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="mb-1 font-semibold">Email</label>
+                        <input
+                            name="fullName"
+                            type="text"
+                            placeholder="Enter your Email"
+                            className="p-2 border border-gray-300 rounded"
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="mb-1 font-semibold">Contact Number</label>
+                        <input
+                            name="fullName"
+                            type="text"
+                            placeholder="Enter your Contact Number"
+                            className="p-2 border border-gray-300 rounded"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className="mb-1 font-semibold">Address</label>
+                        <input
+                            name="address"
+                            type="text"
+                            placeholder="Enter your address"
+                            className="p-2 border border-gray-300 rounded"
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="mb-1 font-semibold">City</label>
+                        <input
+                            name="city"
+                            type="text"
+                            placeholder="Enter your city"
+                            className="p-2 border border-gray-300 rounded"
+                        />
+                    </div>
+                    </form>
+                  
+                  <h2 className="text-2xl font-bold mb-4 mt-8">Summary</h2>
                 <div className="flex justify-between items-center mb-4">
                     <div className="text-lg font-bold">Total</div>
                     <div className="text-2xl font-bold">₱{cart.reduce((total, item) => total + item.product.price * item.quantity, 0)}</div>
                 </div>
                 <button className="w-full bg-black text-white px-6 py-2 rounded-lg hover:bg-gray-800">CHECKOUT</button>
+                </div>    
             </div>
-        </div>
+           
+                  
+                
+              
     );
 }
 
